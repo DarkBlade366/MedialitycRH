9b0184a27111e13c67e7916ea7d302cf2610ce80
DarkBlade <xavierramirezf@gmail.com>
2026-02-07 03:07:09 -0500

    feat: add CreateEmployee and basic funcionality in the Application

---
+++ HumanResource/src/Application/Employees/CreateEmployeeCommand.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain.Enums;
+
+namespace Application.Employees
+{
+    public class CreateEmployeeCommand
+    {
+        public string FullName { get; init; }
+        public string Email { get; init; }
+        public EmployeeRole Role { get; init; }
+    }
+}
---
+++ HumanResource/src/Application/Employees/CreateEmployeeHandler.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Employees
+{
+    public class CreateEmployeeHandler
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        public CreateEmployeeHandler(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+        public async Task<Guid> Handle(CreateEmployeeCommand command)
+        {
+            var employee = new Employee(
+                command.FullName,
+                command.Email,
+                command.Role
+            );
+
+            await _employeeRepository.AddAsync(employee);
+
+            return employee.Id;
+        }
+    }
+}
